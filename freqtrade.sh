ffa4c7d999c010d8979ba8f3336f2db3d91f1d86
Rayiscool1234 <rayan.almoabadi@gmail.com>
2023-04-27 15:59:37 +0300

    Random fix

--- freqtrade.sh
+++ freqtrade.sh
@@ -1,6 +1,6 @@
 $answer
 $version="pre v0.1"
-echo "Do you want docker [Y/n] "
+echo "Do you want docker [y/n] "
 read $answer
 #Switches to docker install than exits
 if [ $answer! = "n" ]; then
@@ -11,12 +11,13 @@ fi
 cd ..
 echo "Updating repository"
 sleep 1
+# Checks if internet exists
 if ping -c 1 archive.ubuntu.com &> /dev/null; then
 	echo ""
 	
 else
-	echo -e "ERR: Internet connection doesn't exist.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router"
-	exit
+	echo -e "ERR: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router"
+	return
 fi
 
 sudo apt-get update
@@ -24,24 +25,26 @@ sudo apt-get update
 # install packages
 echo "Installing Dependencies"
 sleep 1
+# Checks if internet exists
 if ping -c 1 archive.ubuntu.com &> /dev/null; then
 	echo ""
 	
 else
-	echo -e "ERR: Internet connection doesn't exist.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
-	exit
+	echo -e "ERR: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
+	return
 fi
 sudo apt install -y python3-pip python3-venv python3-dev python3-pandas git curl
 
 # Download `develop` branch of freqtrade repository
 echo "Downloading Repository"
 sleep 1
+# Checks if internet exists
 if ping -c 1 github.com &> /dev/null; then
 	echo ""
 	
 else
-	echo -e "ERR: Internet connection doesn't exist.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
-	exit
+	echo -e "ERR: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
+	return
 fi
 git clone https://github.com/freqtrade/freqtrade.git
 
@@ -51,7 +54,7 @@ if [ -d freqtrade ]; then
   echo "Verification successful"
 else
   echo "ERR: Folder doesn't exist!. Installation possibly failed?"
-  exit
+  return
 fi
 echo "Going to dir: /freqtrade"
 sleep 1
@@ -64,6 +67,6 @@ git checkout stable
 ./setup.sh -i
 if [ $? -ne 0 ]; then
 	echo "ERR: Something went wrong, check the above for a possible error"
-	exit
+	return
 fi
 echo -e "You're now ready to run the bot!\nAll you need to do is 'source .env/bin/activate' to enter the enviroment and 'freqtrade --help' and figure out what to do next!"
