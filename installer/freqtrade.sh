e2029e5d18f08d34c6a7e1eaf777dfe7c8240d74
Rayiscool1234 <rayan.almoabadi@gmail.com>
2023-05-05 12:43:02 +0300

    Finished Fixing code

--- installer/freqtrade.sh
+++ installer/freqtrade.sh
@@ -33,107 +33,139 @@ while true; do
 done
 cd ..
 cd ..
-# Docker installation
+# Whether docker installation or regular
 if [ "$docker" -eq 1 ]; then
-	echo "Installing Docker"
+	# Checks if docker already exists
+	if docker --version &> dev/null; then
+		echo "Docker already exists on the System!"
+	else
+		echo "Installing Docker"
+		if ping -c 1 archive.ubuntu.com &> /dev/null; then
+			echo ""
+			else
+			echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router"
+			cd Freqtrade_installer
+			cd installer
+			return
+		fi
+		sudo apt install docker docker-compose &> /dev/null
+		echo "Verifying if Docker installed correctly"
+		com=$(sudo docker run hello-world)
+		DOCKER_ID=$(docker ps -q)
+		sudo docker stop $DOCKER_ID
+			if [ -z "$com" ]; then
+			echo "${RED}ERR${NC}: Docker wasn't installed properly. Installation will be attempt to remove docker"
+			sudo apt-get remove docker docker-engine docker.io containerd runc &> /dev/null
+			echo "Checking if removal was successful"
+			com=$(docker --version)
+			if [ $? -ne 0 ]; then
+				echo "Successful! Exiting installation"
+				cd Freqtrade_installer
+				cd installer
+				return
+			else
+				echo "${DARK_RED}DANGER${NC}: Removal process failed fatally!"
+			cd Freqtrade_installer
+			cd installer
+			return
+			fi
+ 
+		echo "The installation of docker was successful!"
+	fi
+	echo "Creating directory"
+	cd /opt
+	if [ -d "ft_userdata/" ]; then
+		echo "Canceling operation due to already having the directory created"
+	else
+		if sudo mkdir ft_userdata &> dev/null; then
+			echo ""
+		else
+			echo "${RED}ERR${NC}: Unexpected error appeared. $?"
+			echo "Exiting installation (WARNING: THERE MIGHT BE OTHER DIRECTORY THAT WILL BE LEFT BEHIND)"
+			cd ..
+			cd Freqtrade_installer
+			cd installer
+			return
+			
+	cd ft_userdata/
+	echo "Downloading Freqtrade Docker Image"
+	if ping -c 1 raw.githubusercontent.com &> /dev/null; then
+		echo ""
+	else
+		echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router"
+		cd ..
+		cd Freqtrade_installer
+		cd installer
+		return
+	fi
+	sudo curl https://raw.githubusercontent.com/freqtrade/freqtrade/stable/docker-compose.yml -o docker-compose.yml &> /dev/null
+	sudo docker-compose pull &> /dev/null
+else
+	# update repository
+	echo "Updating repository"
+	sleep 1
+	# Checks if internet exists
 	if ping -c 1 archive.ubuntu.com &> /dev/null; then
 		echo ""
-		else
+	else
 		echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router"
 		cd Freqtrade_installer
 		cd installer
 		return
 	fi
-	sudo apt install docker docker-compose
-	echo "Verifying if Docker installed correctly"
-	com=$(sudo docker run hello-world)
-	if [ -z "$com" ]; then
-		echo "${RED}ERR${NC}: Docker wasn't installed properly. Installation will be attempt to remove docker"
-		sudo apt-get remove docker docker-engine docker.io containerd runc &> /dev/null
-		echo "Checking if removal was successful"
-		com=$(docker --version)
-		if [ $? -ne 0 ]; then
-			echo "Successful! Exiting installation"
-			cd Freqtrade_installer
-			cd installer
-			return
-		else
-			echo "${DARK_RED}DANGER${NC}: Removal process failed fatally!"
-			cd Freqtrade_installer
-			cd installer
-			return
-		fi
-
-	else 
-		echo "The installation of docker was successful!"
+	
+	sudo apt-get update &> /dev/null
+	
+	# install packages
+	echo "Installing Dependencies"
+	sleep 1
+	# Checks if internet exists
+	if ping -c 1 archive.ubuntu.com &> /dev/null; then
+		echo ""
+	else
+		echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
+		cd Freqtrade_installer
+		cd installer
+		return
 	fi
-	echo "Downloading Freqtrade Docker Image"
+	sudo apt install -y python3-pip python3-venv python3-dev python3-pandas git curl &> /dev/null
+
+	# Download `develop` branch of freqtrade repository
+	echo "Downloading Repository"
+	sleep 1
+	# Checks if internet exists
+	if ping -c 1 github.com &> /dev/null; then
+		echo ""
+	else
+		echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
+		cd Freqtrade_installer
+		cd installer
+		return
+	fi
+	git clone https://github.com/freqtrade/freqtrade.git &> /dev/null
+
+	# Enter downloaded directory
+	echo "Verifying if directory exists"
+	if [ -d freqtrade ]; then
+		echo "Verification successful"
+	else
+		echo "${RED}ERR${NC}: Folder doesn't exist!. Installation possibly failed to detect an error!? This should be reported to the repository immediately!"
+		cd Freqtrade_installer
+		cd installer
+		return
+	fi
+	echo "Going to dir: /freqtrade"
+	sleep 1
+	cd freqtrade
+
+	# Checks if directory is correct
+	git checkout stable &> /dev/null
+
+	# --install, Install freqtrade from scratch
+	./setup.sh -i
+	if [ $? -ne 0 ]; then
+		echo "${RED}ERR${NC}: Something went wrong, check the above for a possible error"
+		return
+	fi
+	echo -e "You're now ready to run the bot!\nAll you need to do is 'source .env/bin/activate' to enter the enviroment and 'freqtrade --help' and figure out what to do next!"
 fi
-
-# update repository
-echo "Updating repository"
-sleep 1
-# Checks if internet exists
-if ping -c 1 archive.ubuntu.com &> /dev/null; then
-	echo ""
-else
-	echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router"
-	cd Freqtrade_installer
-	cd installer
-	return
-fi
-
-sudo apt-get update
-
-# install packages
-echo "Installing Dependencies"
-sleep 1
-# Checks if internet exists
-if ping -c 1 archive.ubuntu.com &> /dev/null; then
-	echo ""
-else
-	echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
-	cd Freqtrade_installer
-	cd installer
-	return
-fi
-sudo apt install -y python3-pip python3-venv python3-dev python3-pandas git curl
-
-# Download `develop` branch of freqtrade repository
-echo "Downloading Repository"
-sleep 1
-# Checks if internet exists
-if ping -c 1 github.com &> /dev/null; then
-	echo ""
-else
-	echo -e "${RED}ERR${NC}: Internet connection doesn't exist or you have weak internet.\nPlease check your router, ethernet or wifi for any reasons. Few solutions that may assist you:\nconnect the internet\nReboot your router\n\nif all option fail than it is possible that software doesn't exist"
-	cd Freqtrade_installer
-	cd installer
-	return
-fi
-git clone https://github.com/freqtrade/freqtrade.git
-
-# Enter downloaded directory
-echo "Verifying if directory exists"
-if [ -d freqtrade ]; then
-  echo "Verification successful"
-else
-  echo "${RED}ERR${NC}: Folder doesn't exist!. Installation possibly failed to detect an error!? This should be reported to the repository immediately!"
-  cd Freqtrade_installer
-  cd installer
-  return
-fi
-echo "Going to dir: /freqtrade"
-sleep 1
-cd freqtrade
-
-# Checks if directory is correct
-git checkout stable
-
-# --install, Install freqtrade from scratch
-./setup.sh -i
-if [ $? -ne 0 ]; then
-	echo "${RED}ERR${NC}: Something went wrong, check the above for a possible error"
-	return
-fi
-echo -e "You're now ready to run the bot!\nAll you need to do is 'source .env/bin/activate' to enter the enviroment and 'freqtrade --help' and figure out what to do next!"
